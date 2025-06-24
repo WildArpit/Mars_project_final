85d68831faaf418de4dead908524879293ebdd6e
WildArpit <148199806+WildArpit@users.noreply.github.com>
2025-06-25 05:03:34 +0530

    Update document_metadata_pipeline.py

--- document_metadata_pipeline.py
+++ document_metadata_pipeline.py
@@ -1,10 +1,17 @@
 import nltk
+import os
 
-# Ensure required NLTK data is downloaded
+# Force NLTK to use a writable directory
+NLTK_DATA_DIR = "/tmp/nltk_data"
+os.environ["NLTK_DATA"] = NLTK_DATA_DIR
+nltk.data.path.append(NLTK_DATA_DIR)
+
+# Download 'punkt' only if missing
 try:
     nltk.data.find("tokenizers/punkt")
 except LookupError:
-    nltk.download("punkt")
+    nltk.download("punkt", download_dir=NLTK_DATA_DIR)
+
 
 import os
 import pytesseract

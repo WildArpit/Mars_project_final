5fdbfa61c4e101a7d2c6369847be00eccb679cce
WildArpit <148199806+WildArpit@users.noreply.github.com>
2025-06-25 08:28:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,8 @@
-import nltk
-import os
-
-NLTK_DATA_DIR = "/tmp/nltk_data"
-os.environ["NLTK_DATA"] = NLTK_DATA_DIR
-nltk.data.path.append(NLTK_DATA_DIR)
-
-try:
-    nltk.data.find("tokenizers/punkt")
-except LookupError:
-    nltk.download("punkt", download_dir=NLTK_DATA_DIR)
-
-import spacy
-import subprocess
-import importlib.util
 import streamlit as st
 import os
 import tempfile
+import spacy
+import subprocess
 from document_metadata_pipeline import process_document
 
 # Auto-download spaCy model if needed
